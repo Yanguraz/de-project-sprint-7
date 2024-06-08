c27fe6ed7301e148326e25d76186927c5a5f8cd7
Islam Iangurazov <islamyang123@gmail.com>
2024-06-08 15:56:43 +0300

    7-proj-2

--- src/dags/dag_bash.py
+++ src/dags/dag_bash.py
@@ -28,9 +28,9 @@ week_zone_report_hdfs_path = 'hdfs://rc1a-dataproc-m-dg5lgqqm7jju58f9.mdb.yandex
 recommendation_zone_report_hdfs_path = 'hdfs://rc1a-dataproc-m-dg5lgqqm7jju58f9.mdb.yandexcloud.net:8020/user/islamiang/data/datamart/recomendation_zone_report'
 
 with DAG(
-        'datalake_dag',
+        '7nth_project_dag',
         default_args=default_args,
-        description='ETL процесс для даталейка',
+        description='ETL',
         catchup=False,
         schedule_interval='0 2 * * *',
         start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
@@ -40,35 +40,33 @@ with DAG(
     start = DummyOperator(task_id='start')
     
     # Мигрируем гео-события
-    migrate_geo_events_task = BashOperator(
+    migrate_geo_events = BashOperator(
         task_id='migrate_geo_events',
         bash_command=f"spark-submit --master local /lessons/dags/scripts/migration_geo_events.py {execution_date} {raw_events_hdfs_path} {ods_events_hdfs_path}",
         retries=5
     )
 
     # Создаем отчет по зонам пользователей
-    build_user_zone_report_task = BashOperator(
+    build_user_zone_report = BashOperator(
         task_id='build_user_zone_report',
         bash_command=f"spark-submit --master local /lessons/dags/scripts/rpt_user_zone.py {execution_date} {ods_events_hdfs_path} {user_zone_report_hdfs_path}",
         retries=5
     )
     
     # Создаем недельный отчет по зонам
-    build_week_zone_report_task = BashOperator(
+    build_week_zone_report = BashOperator(
         task_id='build_week_zone_report',
         bash_command=f"spark-submit --master local /lessons/dags/scripts/rpt_week_zone.py {execution_date} {ods_events_hdfs_path} {week_zone_report_hdfs_path}",
         retries=5
     )
 
     # Создаем рек отчет по зонам
-    build_recommendation_zone_report_task = BashOperator(
+    build_recommendation_zone_report = BashOperator(
         task_id='build_recommendation_zone_report',
         bash_command=f"spark-submit --master local /lessons/dags/scripts/rpt_rec_zone.py {execution_date} {ods_events_hdfs_path} {recommendation_zone_report_hdfs_path}",
         retries=5
     )
     
-    # Завершающая задача
     finish = DummyOperator(task_id='finish')
     
-    # Определение последовательности выполнения задач
-    start >> migrate_geo_events_task >> [build_user_zone_report_task, build_week_zone_report_task, build_recommendation_zone_report_task] >> finish
+    start >> migrate_geo_events >> [build_user_zone_report, build_week_zone_report, build_recommendation_zone_report] >> finish
